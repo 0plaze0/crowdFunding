19c10187973689f4f040f916700533c24865eb3c
plaze <banikakash131@gmail.com>
2024-04-18 00:46:27 +0530

    feat: add context

---
+++ client/src/context/index.jsx
@@ -0,0 +1,58 @@
+import React, { useContext, createContext } from "react";
+
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+
+import { ethers } from "ethers";
+// import {EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract(
+    "0x406802a2a02B1b9749611bcA90193c41C4AcC8A5"
+  );
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    "createCampaign"
+  );
+
+  const address = useAddress();
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign({
+        args: [
+          address, //owner,
+          form.title,
+          form.description,
+          form.target,
+          new Date(form.deadline).getTime(),
+          form.image,
+        ],
+      });
+    } catch (error) {
+      console.log("contract call failure", error);
+    }
+  };
+
+  return (
+    <StateContext.Provider
+      value={{
+        address,
+        contract,
+        connect,
+        createCampaign: publishCampaign,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
